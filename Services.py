fbc852b5da8824672993e419095b72217887b777
nicolas10405 <53689333+nicolas10405@users.noreply.github.com>
2019-08-08 18:14:31 +0200

    multi round version

--- Services.py
+++ Services.py
@@ -74,7 +74,7 @@ def draw_path(route, short_route, goal, tact, status, auto_rotate=False):
         plt.show()
 
 
-def status_update(number, dubs):
+def status_update(number, dubs, route_length):
 
     progress_number = round(number * (200 / dubs))
     progress_number_100er = 2 * (progress_number // 100)
@@ -83,12 +83,12 @@ def status_update(number, dubs):
     progress_string = "#" + str(progress_number_100er * 100) + "#" * progress_number_remain
 
     print("Step 2 of 2 (Shortening path...): [" + progress_string + "] ",
-          str(number) + " deviations eliminated")
+          str(number) + " deviations eliminated | current path length: " + str(route_length) + " steps")
 
     return time.time()
 
 
-def clean_up_route(long_route):
+def clean_up_route(long_route, hide_stats):
 
     start_time2 = time.time()
     last_execution_time = start_time2 - 0.5
@@ -96,7 +96,7 @@ def clean_up_route(long_route):
     continue_to_check = True
 
     number_of_duplicates_assumed = 500  # len([x for n, x in enumerate(long_route) if x in long_route[:n]])
-    print("\n----Starting path optimization:----")
+    if not hide_stats: print("\n----Starting path optimization:----")
 
     initial_length = len(long_route)
 
@@ -128,99 +128,42 @@ def clean_up_route(long_route):
 
                 status_counter = status_counter + 1
 
-                if time.time() - last_execution_time > 0.5:
-                    last_execution_time = status_update(status_counter, number_of_duplicates_assumed)
+                if time.time() - last_execution_time > 0.5 and not hide_stats:
+                    last_execution_time = status_update(status_counter, number_of_duplicates_assumed, len(long_route))
 
                 break
 
     end_time2 = time.time()
 
-    print("\n----Path shortening completed-----")
-    print(str(round((end_time2 - start_time2) * 1000)), "ms for shortening path")
-    print("From " + str(initial_length) + " steps to " + str(len(long_route)) + " steps (" + str(
-        round((len(long_route) / initial_length) * 100)) + "%) of original\n")
+    if not hide_stats:
+
+        print("\n----Path shortening completed-----")
+        print(str(round((end_time2 - start_time2) * 1000)), "ms for shortening path")
+        print("From " + str(initial_length) + " steps to " + str(len(long_route)) + " steps (" + str(
+            round((len(long_route) / initial_length) * 100)) + "%) of original\n")
 
     return long_route  # now shortend
 
 
-### FOR ANALYSIS
-'''
-def clean_up_route(long_route):
-
-    start_time2 = time.time()
-
-    continue_to_check = True
-
-    while continue_to_check:
-
-        long_route_rev = long_route.copy()
-        long_route_rev.reverse()
-
-        check_list = []
-        check_list.clear()
-
-        for i in range(len(long_route_rev)):
-            if long_route_rev[i] not in check_list:
-                check_list.append(long_route_rev[i])
-                continue_to_check = False
-
-            else:
-                duplicate = long_route_rev[i]
-
-                print("\nDUPE")
-                print(duplicate)
-
-                first_index = long_route.index(duplicate)
-                last_index = len(long_route_rev)-(long_route_rev.index(duplicate))
-
-                print("***Long Route***")
-                print(long_route)
-
-                print("***Long Rev Route***")
-                print(long_route_rev)
-
-                items_to_pop = last_index - (first_index + 1)
-
-                print("Index:", first_index,last_index, "Items:", items_to_pop)
-
-                for i_ in range(items_to_pop):
-                    print(long_route.pop(first_index))
-
-                print ("deletion done")
-                print(long_route)
-
-                continue_to_check = True
-
-                break
-
-
-
-    end_time2 = time.time()
-
-    print("Zeit shortening(ms):", str(round((end_time2 - start_time2) * 1000)))
-
-    return long_route #now shortend
-
-
-
-'''
-
-def save_s_matrix(filename, matrix):
+def save_s_matrix(filename, matrix, hide_stats):
     f = open(str(filename) + ".smx", "wt")
     f.write(str(matrix))
     f.close()
-    print("***Matrix saved to file: " + filename)
+    if not hide_stats: print("***Matrix saved to file: " + filename)
 
 
 def open_s_matrix(filename, matrix):
     if os.path.exists(str(filename) + ".smx"):
         f = open(str(filename) + ".smx", "rt")
+        print("***Matrix opened from file: " + filename + "\nConverting and reading to internal list... (this may take a while)\n")
         matrix = f.read()
         matrix = ast.literal_eval(matrix)
         f.close()
-        print("\n***Matrix opened from file: " + filename)
     else:
-        print("\n***No record found for this configuration")
+        print("\n***No record found for this configuration\n")
     return matrix
 
-
+def round_header(i):
+    print("\n  ------###################-------")
+    print("---###--- Round " + str(i+1) + " started ---###---")
+    print("  ------###################-------\n")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import Setup_Maze
 from numpy.random import choice
+import numpy
 import random
 import Services
 import time
@@ -8,45 +9,60 @@ import sys
 
 # Config (Global)
 
-maxX = 15
+maxX = 25
 maxY = maxX
 maxZ = maxX
 
-w_r_strength_matrix = True
+save_open_s_matrix_to_file = True
 
-debugging_view = False
+debugging_view = False #Lowest level / most details
+debugging_view_L2 = False
+only_show_round_statistics = True
+super_speed = [False, 10]
+
 always_shorten_route = False
+show_visualization = True
 auto_rotate = False
 
-loop = True
+version_for_file_format = "v01"
 
 # Config (Round)
 
-version_for_files = "v01"
+starting_position = [5, 5, 5]
+goal_position = [10, 10, 10]
+distance = abs(starting_position[0] - goal_position[0]) + abs(starting_position[1] - goal_position[1]) + abs(starting_position[2] - goal_position[2])
 
-starting_position = [10, 10, 10]
-goal_position = [2, 2, 2]
-
-abs_reward = 10000 #round(0.25 * maxX * maxY * maxZ) # AI
-reactivate_after = 8  # tacts #AI
-
-route = [starting_position]
-short_route = []
-tact = 1
-status = "NULL"
-
-tact_last_update = 0
-time_last_update = time.time()
-
+learning_factor = 250
+abs_reward = distance * learning_factor  # round(0.25 * maxX * maxY * maxZ) # AI
+reactivate_after = 0  # tacts #AI
 # max_tact_allowed = 10000
-max_tact_allowed = round(0.5 * maxX * maxY * maxZ)  # AI
+max_tact_allowed = round(0.1 * maxX * maxY * maxZ)  # AI
+rounds_to_run = 500
 
+store_for_statistics = []
+last20percent = []
+overall_mean = 0
+median = 0
+p20_mean = 0
+
+# Round initialization
+def round_init(passed_status):
+    global route, short_route, tact, status, tact_last_update, time_last_update
+
+    route = [starting_position]
+    short_route = []
+    tact = 1
+    status = passed_status
+
+    tact_last_update = 0
+    time_last_update = time.time()
+
+
+#round_init("First initialization")
 
 # Services
 
-
 def set_borders():
-
     for y_ in range(maxY):
         for z_ in range(maxZ):
             canGo[maxX][y_][z_] = False
@@ -71,90 +87,49 @@ def get_cg_matrix(x, y, z):
 
 
 def update_strength(total_strength, route):
-
     extra_strength = round(total_strength / len(route))
 
     i = 0
 
-    for i in range(len(route)-1):
-
+    for i in range(len(route) - 1):
         direction = Services.step_direction(route[i], route[i + 1])
         strength[route[i][0]][route[i][1]][route[i][2]][direction[0]][direction[1]] += extra_strength
 
-    print("----Strength updated: +" + str(extra_strength) + " to path----")
-
-''' For Analysis
-
-
-    extra_strength = round(total_strength / tacts)
-
-    print("Extra: " + str(extra_strength))
-    print("routenlänge: " + str(len(route)-1))
-
-    i = 0
-
-    for i in range(len(route)-1):
-
-        direction = Services.step_direction(route[i], route[i + 1])
-
-        print("Runde " + str(i))
-        print(str(direction))
-        print("\n\n")
-        print("Schwarz:")
-
-
-        strength[route[i][0]][route[i][1]][route[i][2]][direction[0]][direction[1]] += extra_strength
-        print("Neuer Wert:" + str(strength[route[i][0]][route[i][1]][route[i][2]][direction[0]][direction[1]]))
-
-        print(str(route[i]) + " did go " + str(direction))
-
-        print(str(strength[route[i][0]][route[i][1]][route[i][2]][direction[0]][direction[1]]))
-
-    print(strength)
-
-    print("Strength updated")
-    
-
-'''
+    if not only_show_round_statistics: print("----Strength updated: +" + str(extra_strength) + " to path----")
 
 
 def test_print(tact_adjuster=0):
-    print("\n----- Tact", tact + tact_adjuster, "------")
-    if tact_adjuster != 0:
-        print("*tact adjustment:", tact_adjuster)
-    print("Status:")
-    print(status)
-    print("Current position:")
-    print(current_position)
+    print("\n----- Tact", tact + tact_adjuster, "(", tact_adjuster, ")------")
+    print("Status: " + status)
+    if debugging_view:
+        print("Current position:")
+        print(current_position)
+        print("CanGO Matrix:")
+        print(current_cg_matrix)
+    print("New Position:")
+    print("-->", new_Position, "(after move in direction " + str(int(random_choice)) + ")")
     print("Strength Matrix:")
     print(current_s_matrix)
-    print("CanGO Matrix:")
-    print(current_cg_matrix)
-    print("New Position:")
-    print(new_Position, "(after move in direction " + str(int(random_choice)) + ")")
-    if not short_route:
-        print("Long Route: " + str(len(route)) + " steps")
-        # print(route, "\n")
-    else:
+    if short_route:
         print("Short Route: " + str(len(short_route)) + " steps")
-        print(short_route, "\n")
+        if debugging_view_L2: print(short_route, "\n")
 
 
 def status_update(percent):
     global time_last_update
     global tact_last_update
 
-    tps = round((tact - tact_last_update)/(time.time()-time_last_update))
+    tps = round((tact - tact_last_update) / (time.time() - time_last_update))
     rt_min = round(((max_tact_allowed - tact) / tps) // 60)
     rt_sec = round(((max_tact_allowed - tact) / tps) % 60)
     print("Step 1 of 2 (Making moves...): [" + "#" * int(percent / 2) + "-" * (50 - int(percent / 2)) + "] ",
-          str(percent) + "% of max, now tact: " + str(tact) + ") - Speed: " + str(tps) + " TPS - max runtime: " + str(rt_min) + " min " + str(rt_sec) + "s")
+          str(percent) + "% of max, now tact: " + str(tact) + ") - Speed: " + str(tps) + " TPS - max runtime: " + str(
+              rt_min) + " min " + str(rt_sec) + "s")
     time_last_update = time.time()
     tact_last_update = tact
 
 
 # Tact
-
 
 def make_a_move(current_position):
     global current_s_matrix, current_cg_matrix
@@ -213,81 +188,115 @@ def make_a_move(current_position):
 
 # Initalisierung
 
-id = Services.generate_id(version_for_files, "_", maxX, maxY, maxZ, starting_position, goal_position)
-
-canGo = Setup_Maze.setupDefaultMazeCG(maxX + 1, maxY + 1, maxZ + 1, defaultValue=True)  # max +1 to give room for 3 boundary planes set to false
+maze_id = Services.generate_id(version_for_file_format, "_", maxX, maxY, maxZ, starting_position, goal_position)
+print("Started " + str(rounds_to_run) + " rounds - Distance: " + str(distance) + " steps - ID: " + maze_id + "\n")
 
 strength = Setup_Maze.setupDefaultMazeS(maxX, maxY, maxZ, defaultValue=100)
+if save_open_s_matrix_to_file: strength = Services.open_s_matrix(maze_id, strength)
 
-if w_r_strength_matrix: strength = Services.open_s_matrix(id, strength)
+for i in range(rounds_to_run):
 
-current_position = starting_position
+    if not only_show_round_statistics: Services.round_header(i)
 
-set_borders()
+    round_init("Round initialization")
 
-status = "\nInitialized round"
+    canGo = Setup_Maze.setupDefaultMazeCG(maxX + 1, maxY + 1, maxZ + 1,
+                                          defaultValue=True)  # max +1 to give room for 3 boundary planes set to false
+    set_borders()
 
-print(status + "\nID: " + id + "\n")
+    current_position = starting_position
 
-print("----Searching goal---- \n(Max: " + str(max_tact_allowed) + " tacts)")
+    status = "Initialized round"
 
-status = "Running"
+    if not only_show_round_statistics: print("----Searching goal---- \n(Max: " + str(max_tact_allowed) + " tacts)")
+    status = "Running"
 
-start_time = time.time()
+    start_time = time.time()
+    div_for_status = round(max_tact_allowed / 50)
 
-div_for_status = round(max_tact_allowed / 50)
+    while tact <= max_tact_allowed:
 
-while tact <= max_tact_allowed:
+        current_position = make_a_move(current_position)
 
-    current_position = make_a_move(current_position)
+        if not current_position:
+            if not only_show_round_statistics: print("\nRound terminated: No more options to move \n")
+            status = "Terminated (Reason: No more options to move)"
+            break
 
-    if not current_position:
-        print("\n*** Round terminated: No more options to move *** \n")
-        status = "Terminated (Reason: No more options to move)"
-        break
+        if current_position == goal_position:
+            status = "Goal found"
+            if not only_show_round_statistics: print(status)
+            long_route = route.copy()
+            if reactivate_after == 0:
+                short_route = route
+            else:
+                short_route = Services.clean_up_route(long_route, only_show_round_statistics)
+            update_strength(abs_reward, short_route)
+            if save_open_s_matrix_to_file: Services.save_s_matrix(maze_id, strength, only_show_round_statistics)
+            break
 
-    if current_position == goal_position:
-        status = "Goal found"
-        print(status)
+        status = "Maximum tact count Reached"
+
+        tact += 1
+
+        if not only_show_round_statistics:
+            if tact % div_for_status == 0 or tact % 25000 == 0:
+                status_update((round((tact / max_tact_allowed) * 100)))
+
+    if status != "Goal found" and always_shorten_route:
         long_route = route.copy()
-        short_route = Services.clean_up_route(long_route)
-        update_strength(abs_reward, short_route)
-        if w_r_strength_matrix: Services.save_s_matrix(id, strength)
-        break
+        short_route = Services.clean_up_route(long_route, only_show_round_statistics)
 
-    status = "Maximum tact count Reached"
+    # Statistics calculation
+    end_time = time.time()
+    ms = (end_time - start_time) * 1000
+    if ms != 0:
+        tps = round(tact / (ms / 1000))
+    else:
+        tps = 0
 
-    tact += 1
+    if not only_show_round_statistics: test_print(-1)
 
-    if tact % div_for_status == 0 or tact % 25000 == 0:
-        status_update((round((tact / max_tact_allowed) * 100)))
+    # Final (always shown) statistics
 
-if status != "Goal found" and always_shorten_route:
-    long_route = route.copy()
-    short_route = Services.clean_up_route(long_route)
+    if short_route:
 
-# Statistics calculation
-end_time = time.time()
-ms = (end_time - start_time) * 1000
-if ms != 0:
-    tps = round(tact / (ms / 1000))
-else:
-    tps = 0
+        length_for_stat = len(short_route) - 1
+        store_for_statistics.append(length_for_stat)
 
-test_print(-1)
+        overall_mean = int(round(numpy.mean(store_for_statistics)))
+        median = int(round(numpy.median(store_for_statistics)))
+        index_last20percent = int(round(max(len(store_for_statistics) / 5, 0)))
+        index_last20percent = len(store_for_statistics) - index_last20percent
 
+        if index_last20percent >= 5:
+            last20percent.clear()
+            last20percent = store_for_statistics[index_last20percent:]
+            p20_mean = int(round(numpy.mean(last20percent)))
+
+    else:
+        length_for_stat = "n/a"
+
+    len_filler = len(str(max_tact_allowed)) - len(str(tact))
+
+    if super_speed[0]:
+        if i % super_speed[1] == 0:
+            print(\
+                "Round: " + str(i + 1) + \
+                " | Tacts: " + len_filler * " " + str(tact) + \
+                " | Shortest route [Avg: " + str(overall_mean) + " Median: " + str(median) + " Avg(L20): " + str(p20_mean) + "] now: " + str(length_for_stat) + \
+                " steps || Runtime: ", round(ms), "ms  | ", tps, "TPS || Status: " + status)
+    else:
+        print( \
+            "Round: " + str(i + 1) + \
+            " | Tacts: " + len_filler * " " + str(tact) + \
+            " | Shortest route [Avg: " + str(overall_mean) + " Median: " + str(median) + " Avg(L20): " + str(
+                p20_mean) + "] now: " + str(length_for_stat) + \
+            " steps || Runtime: ", round(ms), "ms  | ", tps, "TPS || Status: " + status)
 # scratch
 
-# Final (always shown) statistics
-print("Runtime: ", round(ms), "ms  // ", tps, "tacts per second")
-
-#Loop
-
-#if loop: os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
-#os.system(''python C:\Users\nicolas\newermaze\NewerMaze.py'')
-#python = sys.executable
-#os.execv(python, ['python'] + sys.argv)
-
 # Draw route
-Services.draw_path(route, short_route, goal_position, tact, status, auto_rotate)
+if show_visualization: Services.draw_path(route, short_route, goal_position, tact, status, auto_rotate)
 
+print(store_for_statistics)
+#print(strength)
